a5760d02b69a5a2ded439dd104ae039abe1a6f48
Gurudas-Bhardwaj <gursad5@gmail.com>
2024-06-17 20:03:44 +0530

    jsd

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2xes&4qg*j%bdzo75+olsbf^ttf^(b-1rx6^pzz)_h6+05%kc(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.29.99', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 
